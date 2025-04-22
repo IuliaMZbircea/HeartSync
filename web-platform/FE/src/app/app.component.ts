409f0414622fb125255e63d85e8140276f88f6f0
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-04-22 22:57:38 +0300

    Web - Create navbar, presentation page and footer

--- web-platform/FE/src/app/app.component.ts
+++ web-platform/FE/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {PresentationPageComponent} from "./presentation-page/presentation-page.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, PresentationPageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ web-platform/FE/src/app/presentation-page/presentation-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-presentation-page',
+  standalone: true,
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './presentation-page.component.html',
+  styleUrl: './presentation-page.component.css'
+})
+export class PresentationPageComponent {
+
+}
